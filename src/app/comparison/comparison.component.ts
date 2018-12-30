b28cc951e88504213d81cb9f213b1a5c8a36b595
Alex Prozorov <alexprozoroff@gmail.com>
2018-12-30 20:15:36 +0300

    finishing comparison component

--- src/app/comparison/comparison.component.ts
+++ src/app/comparison/comparison.component.ts
@@ -68,7 +68,7 @@ export class ComparisonComponent {
     }
 
     constructor(private api: ApiService, private dataService: DataService, private location: Location) {
-        
+
         if (this.api.currentCity) {
             this.refresh();
         }
@@ -79,41 +79,43 @@ export class ComparisonComponent {
 
     refresh() {
         this.dataService.init(this.api.currentCity.id);
-        
+
         this.location.replaceState('comparison?city=' + this.api.currentCity.id);
-        
+
         this.cities = this.api.getCitiesLookup();
         this.citiesToSelect = Object.assign(this.api.cities);
-        
-        if(!this.citiesToCompare){
-        this.citiesToCompare = [];
 
-         if (this.api.currentCity) {
-            let group;
+        if (!this.citiesToCompare) {
+            this.citiesToCompare = [];
 
-            for (let i = 0, l = this.citiesGroups.length; i < l; i++) {
-                if (this.citiesGroups[i].indexOf(this.api.currentCity.id) > -1) {
-                    group = this.citiesGroups[i];
-                    break;
+            if (this.api.currentCity) {
+                let group;
+
+                for (let i = 0, l = this.citiesGroups.length; i < l; i++) {
+                    if (this.citiesGroups[i].indexOf(this.api.currentCity.id) > -1) {
+                        group = this.citiesGroups[i];
+                        break;
+                    }
                 }
+
+                this.citiesToSelect.map(city => {
+                    if (group.indexOf(city.id) > -1) {
+                        this.citiesToCompare.push(city);
+                    }
+                });
+
+
+            } else {
+                this.citiesToSelect.map(city => this.citiesToCompare.push(city));
             }
-
-            this.citiesToSelect.map(city => {
-                if (group.indexOf(city.id) > -1) {
-                    this.citiesToCompare.push(city);
-                }
-            });
-
-
-        } else {
-            this.citiesToSelect.map(city => this.citiesToCompare.push(city));
-        }
         }
 
         this.citiesString = this.citiesToCompare.map(city => city.name).join(', ');
 
-        let averagePriceComparison = this.dataService.getAveragePriceComparson(),
-            series = [{
+        this.dataService.getAveragePriceComparison().subscribe(values => {
+            const result = Object.keys(values).map(key => [key, values[key]]);
+
+            let series = [{
                 name: '1-комнатные',
                 data: []
             }, {
@@ -123,95 +125,104 @@ export class ComparisonComponent {
                 name: '3-комнатные',
                 data: []
             }],
-            categories = [];
+                categories = [];
 
-        averagePriceComparison.sort((a, b) => {
-            if (a[1][1] > b[1][1]) {
-                return 1;
-            } else {
-                return -1;
-            }
-        });
-
-        for (let i = 0; i < averagePriceComparison.length; i++) {
-            const item = averagePriceComparison[i];
-            if (this.needCompare(item[0])) {
-                series[0].data.push(item[1][0]);
-                series[1].data.push(item[1][1]);
-                series[2].data.push(item[1][2]);
-                categories.push(this.cities[item[0]]);
-            }
-        }
-
-        this.averagePriceChart = {
-            title: { text: 'Средняя стоимость квартиры' },
-            chart: {
-                type: 'column'
-            },
-            xAxis: {
-                type: 'category',
-                labels: { style: { fontSize:'14px' } },
-                categories
-            },
-            yAxis: {
-                title: { text: 'Стоимость, млн. руб.', style: { fontSize:'14px' } },
-                labels: {
-                    style: { fontSize:'14px' },
-                    formatter: function () {
-                        return (this.value / 1000000).toFixed(2);
-                    }
+            result.sort((a, b) => {
+                if (a[1][1] > b[1][1]) {
+                    return 1;
+                } else {
+                    return -1;
                 }
-            },
-            series
-        };
+            });
 
-        let allMeterComparisons = this.dataService.getAverageMeterComparson(),
-            averageMeterComparison = [],
-            meterTitles = [];
-
-        for (let i = 0, l = allMeterComparisons.length; i < l; i++) {
-            const item = allMeterComparisons[i];
-            if (this.needCompare(item[0])) {
-                averageMeterComparison.push(item);
-            }
-        }
-
-        averageMeterComparison.sort((a, b) => {
-            if (a[1] > b[1]) {
-                return 1;
-            } else {
-                return -1;
-            }
-        });
-
-        for (let i = 0; i < averageMeterComparison.length; i++) {
-            const item = averageMeterComparison[i];
-            meterTitles.push(this.cities[item[0]]);
-        }
-
-        this.averageMeterChart = {
-            title: { text: 'Средняя стоимость квадратного метра' },
-            chart: {
-                type: 'column'
-            },
-            xAxis: {
-                type: 'category',
-                labels: { style: { fontSize:'14px' } },
-                categories: meterTitles
-            },
-            yAxis: {
-                title: { text: 'Стоимость, тыс. руб.', style: { fontSize:'14px' }  },
-                labels: {
-                    style: { fontSize:'14px' } ,
-                    formatter: function () {
-                        return Math.round(this.value / 1000);
-                    }
+            for (let i = 0; i < result.length; i++) {
+                const item = result[i];
+                if (this.needCompare(item[0])) {
+                    series[0].data.push(item[1][0]);
+                    series[1].data.push(item[1][1]);
+                    series[2].data.push(item[1][2]);
+                    categories.push(this.cities[item[0]]);
                 }
-            },
-            series: [
-                { data: averageMeterComparison, name: 'Квадратный метр' }
-            ]
-        };
+            }
+
+            this.averagePriceChart = {
+                title: { text: 'Средняя стоимость квартиры' },
+                chart: {
+                    type: 'column'
+                },
+                xAxis: {
+                    type: 'category',
+                    labels: { style: { fontSize: '14px' } },
+                    categories
+                },
+                yAxis: {
+                    title: { text: 'Стоимость, млн. руб.', style: { fontSize: '14px' } },
+                    labels: {
+                        style: { fontSize: '14px' },
+                        formatter: function () {
+                            return (this.value / 1000000).toFixed(2);
+                        }
+                    }
+                },
+                series
+            };
+
+
+        })
+
+        this.dataService.getAverageMeterComparson().subscribe(values => {
+
+            const result = Object.keys(values).map(key => {
+                return [key, values[key].reduce((a, b) => a + b, 0) / (values[key].length)];
+            })
+
+            let averageMeterComparison = [],
+                meterTitles = [];
+
+            for (let i = 0, l = result.length; i < l; i++) {
+                const item = result[i];
+                if (this.needCompare(item[0])) {
+                    averageMeterComparison.push(item);
+                }
+            }
+
+            averageMeterComparison.sort((a, b) => {
+                if (a[1] > b[1]) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            });
+
+            for (let i = 0; i < averageMeterComparison.length; i++) {
+                const item = averageMeterComparison[i];
+                meterTitles.push(this.cities[item[0]]);
+            }
+
+            this.averageMeterChart = {
+                title: { text: 'Средняя стоимость квадратного метра' },
+                chart: {
+                    type: 'column'
+                },
+                xAxis: {
+                    type: 'category',
+                    labels: { style: { fontSize: '14px' } },
+                    categories: meterTitles
+                },
+                yAxis: {
+                    title: { text: 'Стоимость, тыс. руб.', style: { fontSize: '14px' } },
+                    labels: {
+                        style: { fontSize: '14px' },
+                        formatter: function () {
+                            return Math.round(this.value / 1000);
+                        }
+                    }
+                },
+                series: [
+                    { data: averageMeterComparison, name: 'Квадратный метр' }
+                ]
+            };
+
+        })
     }
-
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -5,28 +5,12 @@ import { HttpClient } from '@angular/common/http';
 export class DataService {
 
     private city: string;
-    private average_price: any;
-    private room_count_ratio: any;
-
-    private average_price_comparison: any;
-    private average_meter_comparison: any;
 
     constructor(private http: HttpClient){}
 
     init(city?: string) {
-
-        if (!city) {
-            return;
-        }
-
-        if (this.city === city) {
-            return;
-        }
-
+        if (!city || this.city === city) return;
         this.city = city;
-
-        // this.loadData('average_price_comparison');
-        // this.loadData('average_meter_comparison');
     }
 
     loadJSON(dateId: string, city?: string, ) {
@@ -42,61 +26,40 @@ export class DataService {
         return this.http.get(path);
     }
 
-    loadData(dataId: string) {
-        return this.loadJSON(dataId, this.city);
+    loadData(dataId: string, cityId?: string) {
+        return this.loadJSON(dataId, cityId);
     }
 
-    getAveragePriceComparson(): any {
-        const data = this.average_price_comparison,
-            keys = Object.keys(data);
-
-        let result = [];
-
-        for (let i = 0, l = keys.length; i < l; i++) {
-            const key = keys[i];
-            result.push([key, data[key]]);
-        }
-
-        return result;
+    getAveragePriceComparison(): any {
+        return this.loadData('average_price_comparison');
     }
 
     getAverageMeterComparson(): any {
-        const data = this.average_meter_comparison,
-            keys = Object.keys(data);
-
-        let result = [],
-            accuracyFactor = 100;
-
-        for (let i = 0, l = keys.length; i < l; i++) {
-            const key = keys[i];
-            result.push([key, Math.round(data[key].reduce((a, b) => a + b, 0) / (data[key].length * accuracyFactor)) * accuracyFactor]);
-        }
-
-        return result;
+        return this.loadData('average_meter_comparison');
     }
 
     getAveragePrice(): any {
-        return this.loadData('average_price');
+        return this.loadData('average_price', this.city);
     }
 
     getRoomCountRatio(): any {
-        return this.loadData('room_count_ratio');
+        return this.loadData('room_count_ratio', this.city);
     }
 
     getMeterPrice(): any {
-        return this.loadData('average_price_by_meter');
+        return this.loadData('average_price_by_meter', this.city);
     }
 
     getPriceByDistrict(): any {
-        return this.loadData('price_by_district');
+        return this.loadData('price_by_district', this.city);
     }
 
     getVolumeByDistrict(): any {
-        return this.loadData('volume_by_district');
+        return this.loadData('volume_by_district', this.city);
     }
 
     getPriceByConstructionType(): any {
-        return this.loadData('price_by_construction_type');
+        return this.loadData('price_by_construction_type', this.city);
     }
 
 }
